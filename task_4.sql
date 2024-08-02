d375ff4df6b72adef8c8f5a3a12ffa4fa58274ff
KAALI-M <163420046+KAALI-M@users.noreply.github.com>
2024-08-02 17:53:32 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,14 @@
--- Use the specified database
-USE alx_book_store;
+USE information_schema;
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM columns
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME='books';
 
--- Show the full description of the books table
-SHOW FULL COLUMNS FROM books;
+
+
